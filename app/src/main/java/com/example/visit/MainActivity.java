adc59ae4963d4ed54624151a90646ecb98f39f1b
PSMmissy <vkeo3134@naver.com>
2021-06-28 17:37:13 +0900

    하이

--- app/src/main/java/com/example/visit/MainActivity.java
+++ app/src/main/java/com/example/visit/MainActivity.java
@@ -1,48 +1,85 @@
 package com.example.visit;
 
-import android.app.Activity;
-import android.graphics.Point;
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.Display;
-import android.widget.TextView;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.Button;
+import android.widget.HorizontalScrollView;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
+    LinearLayout layout;
+    HorizontalScrollView scrollView;
+    boolean isInit;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    public Point getScreenSize(Activity activity) {
-        Display display = activity.getWindowManager().getDefaultDisplay();
-        Point size = new Point();
-        display.getSize(size);
-        TextView textView = (TextView)findViewById(R.id.textView);
-        textView.setSingleLine(true); //한줄로 나오게 하기.
-        textView.setEllipsize(TextUtils.TruncateAt.MARQUEE);//Ellipsize의 MARQUEE 속성 주기
-        textView.setSelected(true);
 
+        isInit = true;
 
-        출처: https://commin.tistory.com/23 [Commin의 일상코딩]
-
-        return  size;
+        layout=findViewById(R.id.scroll_layout);
+        scrollView = findViewById(R.id.scrollview);
     }
 
-    int standardSize_X, standardSize_Y;
-    float density;
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
 
-    public void getStandardSize() {
-        Point ScreenSize = getScreenSize(this);
-        density  = getResources().getDisplayMetrics().density;
+        if (isInit) {
+            isInit = false;
+            int but = (scrollView.getWidth() - (int) dpToPx(this, 2)) / 3;
+            int ba = (scrollView.getHeight()/3);
+            Log.e("but", "layout " + scrollView.getWidth() + " " + "but " + but);
+            for (int i = 0; i < 9; i++) {
+                if (i % 2 == 0) {
+                    Button button = new Button(this);
+                    LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(but, LinearLayout.LayoutParams.MATCH_PARENT);
+                    if(i==0){
+                        button.setText("전시/공연");
+                    }
+                    else if(i==2){
+                        button.setText("체험놀이");
+                    }
+                    else if(i==4){
+                        button.setText("예약관리자");
+                    }
+                    else if(i==6){
+                        button.setText("일단");
+                    }
+                    else{
+                        button.setText("아무거나");
+                    }
+                    params.setMargins(0, 0, 0, 0);
+                    button.setTextColor(Color.parseColor("#ffffff"));
+                    button.setLayoutParams(params);
+                    button.setBackground(null);
+                    layout.addView(button);
 
-        standardSize_X = (int) (ScreenSize.x / density);
-        standardSize_Y = (int) (ScreenSize.y / density);
+                } else {
+                    View bar = new View(this);
+
+                    LinearLayout.LayoutParams params = new LinearLayout.LayoutParams((int) dpToPx(this, 1), LinearLayout.LayoutParams.MATCH_PARENT);
+                    params.setMargins(0, ba, 0, ba);
+                    bar.setBackgroundResource(R.drawable.block);
+                    bar.setLayoutParams(params);
+                    layout.addView(bar);
+                }
+            }
+        }
     }
 
-
-
+    public float dpToPx(Context context, float dp) {
+        DisplayMetrics dm = context.getResources().getDisplayMetrics();
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, dm);
+    }
 }
